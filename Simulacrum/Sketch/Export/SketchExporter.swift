1069a16c3bd7945724283e1a2b792652d8123517
Martin Johannesson <mjohannesson@gmail.com>
2020-04-12 14:17:59 +0200

    Export to Sketch file format directly.

--- Simulacrum/Sketch/Export/SketchExporter.swift
+++ Simulacrum/Sketch/Export/SketchExporter.swift
@@ -5,8 +5,13 @@
 import Foundation
 import SystemKit
 import Cocoa
+import Zipcode
 
 class SketchExporter {
+    
+    enum Error: Swift.Error {
+        case failedToWriteData
+    }
     
     struct Artboard {
         let id: String
@@ -23,79 +28,109 @@ class SketchExporter {
             .appendingComponent("SketchExport")
             .appendingComponent(UUID().uuidString)
             try exportFolder.createDirectory(withIntermediateDirectories: true)
-
-        let documentFolder = exportFolder
-            .appendingComponent("Screenshots \(dateFormatter.string(from: Date()))")
-        try documentFolder.createDirectory(withIntermediateDirectories: true)
-
-        let pagesFolder = documentFolder.appendingComponent("pages")
-        try pagesFolder.createDirectory(withIntermediateDirectories: true)
-        
-        let imagesFolder = documentFolder.appendingComponent("images")
-        try imagesFolder.createDirectory(withIntermediateDirectories: true)
         
         let pageID = UUID().uuidString
         
-        var artboards: [Artboard] = []
+        let archive = ZipArchive(path: exportFolder.appendingComponent("Screenshots \(dateFormatter.string(from: Date())).sketch").string)
+        try archive.write(type: .overwrite) { writer in
         
-        var y: Int = 0
-        
-        for (_, screenshotFiles) in screenshots.screenshotsByDeviceName {
-            var x: Int = 0
-            var maxHeight: Int = 0
-            for screenshot in screenshotFiles {
-                
-                guard let image = NSImage(contentsOf: screenshot.path.url) else {
-                    print("ERROR: Could not read \(screenshot.path.string)")
-                    continue
-                }
-                
-                let (width, height) = image.sizeInPixels
-                
-                maxHeight = max(maxHeight, height)
+            var artboards: [Artboard] = []
+            
+            var y: Int = 0
+            
+            for (_, screenshotFiles) in screenshots.screenshotsByDeviceName {
+                var x: Int = 0
+                var maxHeight: Int = 0
+                for screenshot in screenshotFiles {
+                    
+                    guard let image = NSImage(contentsOf: screenshot.path.url) else {
+                        print("ERROR: Could not read \(screenshot.path.string)")
+                        continue
+                    }
+                    
+                    let (width, height) = image.sizeInPixels
+                    
+                    maxHeight = max(maxHeight, height)
 
-                let imageFileName = String((UUID().uuidString + UUID().uuidString)
-                    .lowercased().prefix(40))
-                let artboardName = screenshot.deviceName
-                let artboardID = UUID().uuidString
-                let artboardJSON = makeArtboardJSON(x: x,
-                                                    y: y,
-                                                    width: width,
-                                                    height: height,
-                                                    imageName: artboardName,
-                                                    imageFileName: imageFileName,
-                                                    artboardName: artboardName,
-                                                    artboardID: artboardID,
-                                                    pageID: pageID)
-                let artboard = Artboard(id: artboardID,
-                                        name: artboardName,
-                                        json: artboardJSON)
-                artboards.append(artboard)
-                
-                let imagePath = imagesFolder
-                    .appendingComponent(imageFileName)
-                    .appendingExtension("png")
-                
-                try image.write(to: imagePath.url)
-                
-                x += 200 + width
+                    let imageFileName = String((UUID().uuidString + UUID().uuidString)
+                        .lowercased().prefix(40))
+                    let artboardName = screenshot.deviceName
+                    let artboardID = UUID().uuidString
+                    let artboardJSON = makeArtboardJSON(x: x,
+                                                        y: y,
+                                                        width: width,
+                                                        height: height,
+                                                        imageName: artboardName,
+                                                        imageFileName: imageFileName,
+                                                        artboardName: artboardName,
+                                                        artboardID: artboardID,
+                                                        pageID: pageID)
+                    let artboard = Artboard(id: artboardID,
+                                            name: artboardName,
+                                            json: artboardJSON)
+                    artboards.append(artboard)
+                                        
+                    try writer.writeEntryNamed("images/\(imageFileName).png", image: image)
+                    
+                    x += 200 + width
+                }
+                y += 500 + maxHeight
             }
-            y += 500 + maxHeight
+
+            let pageJSON = makePageJSON(pageID: pageID, artboards: artboards)
+
+            let documentJSON = makeDocumentJSON(pageID: pageID)
+
+            let metaJSON = makeMetaJSON(pageID: pageID, artboards: artboards)
+
+            let userJSON = makeUserJSON(pageID: pageID)
+                    
+            /*
+            try metaJSON.write(to: documentFolder.appendingComponent("meta.json").url, atomically: true, encoding: .utf8)
+            try userJSON.write(to: documentFolder.appendingComponent("user.json").url, atomically: true, encoding: .utf8)
+            try documentJSON.write(to: documentFolder.appendingComponent("document.json").url, atomically: true, encoding: .utf8)
+            try pageJSON.write(to: pagesFolder.appendingComponent("\(pageID).json").url, atomically: true, encoding: .utf8)*/
+        
+            try writer.writeEntryNamed("meta.json", string: metaJSON)
+            try writer.writeEntryNamed("user.json", string: userJSON)
+            try writer.writeEntryNamed("document.json", string: documentJSON)
+            try writer.writeEntryNamed("pages/\(pageID).json", string: pageJSON)
         }
 
-        let pageJSON = makePageJSON(pageID: pageID, artboards: artboards)
-
-        let documentJSON = makeDocumentJSON(pageID: pageID)
-
-        let metaJSON = makeMetaJSON(pageID: pageID, artboards: artboards)
-
-        let userJSON = makeUserJSON(pageID: pageID)
-                
-        try metaJSON.write(to: documentFolder.appendingComponent("meta.json").url, atomically: true, encoding: .utf8)
-        try userJSON.write(to: documentFolder.appendingComponent("user.json").url, atomically: true, encoding: .utf8)
-        try documentJSON.write(to: documentFolder.appendingComponent("document.json").url, atomically: true, encoding: .utf8)
-        try pageJSON.write(to: pagesFolder.appendingComponent("\(pageID).json").url, atomically: true, encoding: .utf8)
-
         NSWorkspace.shared.open(exportFolder.url)
+    }
+}
+
+private extension ZipWriter {
+    
+    func writeEntryNamed(_ name: String, string: String) throws {
+        let data = string.data(using: .utf8) ?? Data()
+        try writeEntryNamed(name, data: data)
+    }
+}
+
+private extension ZipWriter {
+    
+    enum NSImageZipWriterError: Error {
+        case failedToGetTIFFRepresentation
+        case failedToCreateBitmapRepresentation
+        case failedToCreateImageData
+    }
+    
+    func writeEntryNamed(_ name: String, image: NSImage, fileType: NSBitmapImageRep.FileType = .png) throws {
+
+        guard let tiffData = image.tiffRepresentation else {
+            throw NSImageZipWriterError.failedToGetTIFFRepresentation
+        }
+        
+        guard let bitmap = NSBitmapImageRep(data: tiffData) else {
+            throw NSImageZipWriterError.failedToCreateBitmapRepresentation
+        }
+        
+        guard let data = bitmap.representation(using: fileType, properties: [.compressionFactor: 1.0]) else {
+            throw NSImageZipWriterError.failedToCreateImageData
+        }
+
+        try writeEntryNamed(name, data: data)
     }
 }
